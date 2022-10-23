fcc0fb2f06db2c5d1611644129948613d5fcb4bd
msraees <115069041+msraees@users.noreply.github.com>
2022-10-23 17:43:57 +0530

    oprators
    oprators

---
+++ Oprators/index.js
@@ -0,0 +1,12 @@
+//<--------Arithmetic Oprators-------->
+var a = 40;
+var b = 30;
+var c = a + b; //Additio Oprator-------------->
+var d = a - b; //Subtraction Oprator---------->
+var e = a * b; //Multiplication Oprator------->
+var f = a / b; //Division Oprator------------->
+var g = a % b; //Modulus(Division Remainder)-->
+a++;           //Increment Oprator------------>
+b--;           //Dicrement Oprator------------>
+var h = a**b;  //Exponentiation(ES2016)------->
+console.log(a, b, c, d, e, f, g, h)
